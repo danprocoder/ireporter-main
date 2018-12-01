71171216c0c291c13489b1176d0297319953e624
Daniel Austin <danielaustin718@gmail.com>
2018-12-01 14:28:54 +0100

    created basic routes

---
+++ libraries/response.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+  success: function(data) {
+    return {
+      'status': 200,
+      'data': data instanceof Array ? data : [data]
+    };
+  },
+
+  fail: function(message) {
+    return {
+      'status': 404,
+      'error': message
+    };
+  }
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  // ireporter home page.
+});
+
+app.get('/signup', (req, res) => {
+
+});
+app.post('/signup', (req, res) => {
+
+});
+
+app.get('/login', (req, res) => {
+
+});
+app.post('/login', (req, res) => {
+
+});
+
+// APIs
+// Version 1
+app.get('/api/v1/red-flags', (req, res) => {
+
+});
