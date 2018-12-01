ecb02a3e78fc47938ab573cb92cff7e3868b015e
Daniel Austin <danielaustin718@gmail.com>
2018-12-01 15:27:46 +0100

    created route for login and signup

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/frontend/index.html');
+  res.sendFile(path.join(__dirname, '/frontend/index.html'));
 });
 
 app.get('/signup', (req, res) => {
-
+  res.sendFile(path.join(__dirname, '/frontend/signup.html'));
 });
 app.post('/signup', (req, res) => {
 
 });
 
 app.get('/login', (req, res) => {
-
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 app.post('/login', (req, res) => {
 
