7d3505d0b0f1b1e97828ba1755140926d635ba05
Daniel Austin <danielaustin718@gmail.com>
2018-12-01 15:30:30 +0100

    .

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/index.html'));
 });
 
 app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/signup.html'));
+  res.sendFile(path.join(__dirname, 'frontend/signup.html'));
 });
 app.post('/signup', (req, res) => {
 
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
+  res.sendFile(path.join(__dirname, 'frontend/login.html'));
 });
 app.post('/login', (req, res) => {
 
