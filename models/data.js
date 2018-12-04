eb45e85cffef611042569b66254aa7b3aba3931f
Daniel Austin <danielaustin718@gmail.com>
2018-12-04 14:39:50 +0100

    feat(dataFile): created class to read and write files. [Delivers #457885]

---
+++ models/data.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import fileSystem, { mkdirSync, fstat } from 'fs';
+import path from 'path';
+
+class Data {
+  constructor(name) {
+    var relPath = path.join(__dirname, 'data');
+
+    if (!fileSystem.existsSync(relPath)) {
+      mkdirSync(relPath);
+    }
+
+    var filePath = path.join(relPath, name);
+    if (!fileSystem.existsSync(filePath)) {
+        fileSystem.writeFileSync(filePath, '{}');
+        this.data = {};
+    } else {
+        let data = fileSystem.readFileSync(filePath);
+        this.data = JSON.parse(data);
+    }
+  }
+
+  insert(data) {
+  }
+
+  update() {
+
+  }
+
+  delete(id) {
+
+  }
+
+  getAll() {
+
+  }
+
+  get(id) {
+
+  }
+}
+
+export default Data;
