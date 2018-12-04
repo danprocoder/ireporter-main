0d83b47eda82c24dda57484470814a4d323dd832
Daniel Austin <danielaustin718@gmail.com>
2018-12-04 13:37:05 +0100

    Changed from ES5 to ES6

--- libraries/response.js
+++ libraries/response.js
@@ -1,16 +1,17 @@
-module.exports = {
-  
-  success: function(data) {
+'use strict';
+
+class Response {
+  success(data) {
     return {
       'status': 200,
       'data': data instanceof Array ? data : [data]
-    };
-  },
+    }
+  }
 
-  fail: function(message) {
+  fail(message) {
     return {
       'status': 404,
       'error': message
-    };
+    }
   }
 }
