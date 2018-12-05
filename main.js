6535565bb0eda6b31bedfaef0d46d033f5c217fe
Daniel Austin <danielaustin718@gmail.com>
2018-12-05 15:47:47 +0100

    chore(updateMainJs): Removed 'use strict' and used express.json() [Finishes #12341212]

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-'use strict';
-
 import express from 'express';
 import path from 'path';
-import bodyParser from 'body-parser';
 import load from './loader.js';
 
 const app = express();
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/index.html'));
