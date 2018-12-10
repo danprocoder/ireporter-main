82262452d369be4335a3bac485e1dd597ceef739
Daniel Austin <danielaustin718@gmail.com>
2018-12-10 17:04:15 +0100

    chore(routes, incidentController): refactored code and files - split routes into different files - created api-endpoints/config.js - renamed red-flag.js to Incident.js [Finishes #162473110]

---
+++ api-endpoints/config.js
@@ -0,0 +1,11 @@
+export default {
+  jwt: {
+    secret: '1h0e1h98e2hq89hef0q9nw9fandnfoqhe9r8h39qn9f8ef98q38h98*(H*(H(h9eh9q8h0q)))'
+  },
+  database: {
+    host: 'localhost',
+    user: 'daniel',
+    pass: 'daniel',
+    port: 5432
+  }
+}
--- api-endpoints/main.js
+++ api-endpoints/main.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
-import apiRouter from './src/routes/api';
+import userRouter from './src/routes/users';
+import redFlagRouter from './src/routes/red-flags';
+import interventionRouter from './src/routes/interventions';
 
 const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.use(apiRouter);
+app.use(userRouter);
+app.use(redFlagRouter);
+app.use(interventionRouter);
 app.use(express.static(path.join(__dirname, '../UI')));
 
 app.listen(process.env.PORT || 8001, () => {
