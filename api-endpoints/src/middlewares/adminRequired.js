e38902e6500fcf4ca2334d0a7211d9827c4da6d4
Daniel Austin <danielaustin718@gmail.com>
2018-12-13 14:11:28 +0100

    Few modifications

--- api-endpoints/src/middlewares/adminRequired.js
+++ api-endpoints/src/middlewares/adminRequired.js
@@ -2,7 +2,7 @@ import response from '../helpers/response';
 
 export default (req, res, next) => {
   if (!req.loggedInUser.isadmin) {
-    res.status(400).json(response.fail('User must be an admin'));
+    res.status(403).json(response.fail('User must be an admin'));
   } else {
     next();
   }
